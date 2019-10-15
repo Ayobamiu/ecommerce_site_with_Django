20230037bd9b84a742781992eb8c170b84e91a59
Ayobami Usman <49147442+Ayobamiu@users.noreply.github.com>
2019-10-15 16:34:26 +0100

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Product, Offer
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+admin.site.register(Product, ProductAdmin)
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'discount')
+admin.site.register(Offer, OfferAdmin)
+
